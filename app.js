de0cbed97d91c8afa713b18e5d579ad9afee14de
rilweezy120 <50751857+rilweezy120@users.noreply.github.com>
2023-09-11 16:47:06 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+
+// ********** set date ************
+// const date = document.getElementById('date')
+const day = document.getElementById('day');
+const time = document.getElementById('time');
+
+
+
+const dt = new Date();
+// date.textContent = dt.getFullYear();
+day.textContent = dt.toLocaleDateString('en-EN', { weekday: 'long' });
+time.textContent = dt.getTime();
+
+window.addEventListener('scroll', () => {
+    const navBar = document.getElementById('nav')
+    const topBtn = document.querySelector('.top-link')
+    const scrollHeight = window.scrollY;
+
+    if(scrollHeight > 2){
+        topBtn.classList.add('show-link');
+    }
+    else{
+        topBtn.classList.remove('show-link')
+    }
+
+})
